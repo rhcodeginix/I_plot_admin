5a4da9952e9b24317f875898128407328d238fd6
Drashti Savani <rhshukla124@gmail.com>
2025-07-07 10:51:55 +0530

    add some change

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,12 +1,26 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useIsAuthenticated } from "../hooks/useAuth";
 import { useEffect, useState } from "react";
 import { fetchAdminDataByEmail } from "../lib/utils";
+import { analytics } from "../config/firebaseConfig";
+import { logEvent } from "firebase/analytics";
 
 export const AuthLayout = () => {
   const isAuthenticated = useIsAuthenticated();
   const [role, setRole] = useState<string | null>(null);
   const [loading, setLoading] = useState(true);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    if (analytics) {
+      logEvent(analytics, "page_view", {
+        page_path: location.pathname + location.search,
+        page_location: window.location.href,
+        page_title: document.title,
+      });
+    }
+  }, [location]);
 
   useEffect(() => {
     const getData = async () => {

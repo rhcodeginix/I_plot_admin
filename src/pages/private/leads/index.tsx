f171284e694b7b6f544e45f6de2127ece04d0b7e
Drashti Savani <rhshukla124@gmail.com>
2025-07-23 17:47:09 +0530

    add some change

--- src/pages/private/leads/index.tsx
+++ src/pages/private/leads/index.tsx
@@ -13,14 +13,14 @@ export const AllLeads = () => {
             </h1>
             <p className="text-gray">Liste over alle Leads</p>
           </div>
-          <div className="flex gap-3">
+          {/* <div className="flex gap-3">
             <Button
               text="Legg til"
               className="border border-purple bg-purple text-white text-sm rounded-[8px] h-[40px] font-medium relative px-3 md:px-4 py-[10px] flex items-center gap-2"
               icon={<Plus className="text-white w-5 h-5" />}
               path="/add-agent-leads"
             />
-          </div>
+          </div> */}
         </div>
         <LeadTable />
       </div>

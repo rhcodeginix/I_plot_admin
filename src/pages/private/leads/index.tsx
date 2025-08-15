ec8e4ebfcfff8c3576a82f21ee3ce0b3225e0786
Drashti Savani <rhshukla124@gmail.com>
2025-08-15 15:33:38 +0530

    add some change

--- src/pages/private/leads/index.tsx
+++ src/pages/private/leads/index.tsx
@@ -1,8 +1,26 @@
 import { Plus } from "lucide-react";
 import Button from "../../../components/common/button";
 import { LeadTable } from "./leadTable";
+import { useEffect, useState } from "react";
+import { fetchAdminDataByEmail } from "../../../lib/utils";
 
 export const AllLeads = () => {
+  const [Role, setRole] = useState<any>(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await fetchAdminDataByEmail();
+
+      if (data) {
+        if (data?.role) {
+          setRole(data?.role);
+        }
+      }
+    };
+
+    getData();
+  }, []);
+
   return (
     <>
       <div className="px-6 pt-6 pb-16 flex flex-col gap-6">
@@ -13,14 +31,16 @@ export const AllLeads = () => {
             </h1>
             <p className="text-gray">Liste over alle Leads</p>
           </div>
-          <div className="flex gap-3">
-            <Button
-              text="Registrer nytt tips"
-              className="border border-purple bg-purple text-white text-sm rounded-[8px] h-[40px] font-medium relative px-3 md:px-4 py-[10px] flex items-center gap-2"
-              icon={<Plus className="text-white w-5 h-5" />}
-              path="/add-bank-leads"
-            />
-          </div>
+          {Role && Role !== "Bankansvarlig" && (
+            <div className="flex gap-3">
+              <Button
+                text="Registrer nytt tips"
+                className="border border-purple bg-purple text-white text-sm rounded-[8px] h-[40px] font-medium relative px-3 md:px-4 py-[10px] flex items-center gap-2"
+                icon={<Plus className="text-white w-5 h-5" />}
+                path="/add-bank-leads"
+              />
+            </div>
+          )}
         </div>
         <LeadTable />
       </div>

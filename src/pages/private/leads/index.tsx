da8deefbee20c32815e3fd79035b52cd4c6d1523
Drashti Savani <rhshukla124@gmail.com>
2025-08-16 19:06:05 +0530

    add some change

--- src/pages/private/leads/index.tsx
+++ src/pages/private/leads/index.tsx
@@ -1,4 +1,4 @@
-import { MoveLeft, Plus } from "lucide-react";
+import { Plus } from "lucide-react";
 import Button from "../../../components/common/button";
 import { LeadTable } from "./leadTable";
 import { useEffect, useState } from "react";
@@ -30,36 +30,36 @@ export const AllLeads = () => {
   return (
     <>
       <div className="px-6 pt-6 pb-16 flex flex-col gap-6">
-        {Supplier && Supplier === "9f523136-72ca-4bde-88e5-de175bc2fc71" && (
-          <div
-            className="text-primary text-sm md:text-base lg:text-lg font-semibold flex items-center gap-4 cursor-pointer"
-            onClick={() => {
-              const url = `https://boligkonfigurator.mintomt.no?email=${encodeURIComponent(
-                Email
-              )}`;
-              window.location.href = url;
-            }}
-          >
-            <MoveLeft /> Gå til romkonfiguratoren
-          </div>
-        )}
-        <div className="flex items-center justify-between">
+        <div className="flex flex-col md:flex-row md:items-center justify-between gap-5">
           <div className="flex flex-col gap-1">
             <h1 className="text-darkBlack font-medium text-xl md:text-2xl desktop:text-[30px]">
               Tips til bank
             </h1>
             <p className="text-gray">Liste over alle Leads</p>
           </div>
-          {Role && Role !== "Bankansvarlig" && (
-            <div className="flex gap-3">
+          <div className="flex gap-4 flex-col sm:flex-row sm:items-center">
+            {Supplier &&
+              Supplier === "9f523136-72ca-4bde-88e5-de175bc2fc71" && (
+                <Button
+                  text="Gå til romkonfiguratoren"
+                  className="border-2 border-purple text-purple text-sm rounded-[8px] h-[40px] font-medium relative px-4 py-[10px] flex items-center gap-2"
+                  onClick={() => {
+                    const url = `https://boligkonfigurator.mintomt.no?email=${encodeURIComponent(
+                      Email
+                    )}`;
+                    window.location.href = url;
+                  }}
+                />
+              )}
+            {Role && Role !== "Bankansvarlig" && (
               <Button
                 text="Registrer nytt tips"
                 className="border border-purple bg-purple text-white text-sm rounded-[8px] h-[40px] font-medium relative px-3 md:px-4 py-[10px] flex items-center gap-2"
                 icon={<Plus className="text-white w-5 h-5" />}
                 path="/add-bank-leads"
               />
-            </div>
-          )}
+            )}
+          </div>
         </div>
         <LeadTable />
       </div>
